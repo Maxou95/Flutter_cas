527a880468c0ee68010b621c14d9564b8f058271
Maxou95 <rougetmaxime0@gmail.com>
2022-01-23 21:57:50 +0100

    dernier commit

--- lib/screen/info.dart
+++ lib/screen/info.dart
@@ -1,16 +1,45 @@
-///
-/// screen/home.dart
-///
 import 'package:flutter/material.dart';
 
-class InfosScreen extends StatelessWidget {
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import 'package:cv/model/airtable_infos.dart';
+
+class InfosScreen extends StatefulWidget {
+  const InfosScreen({Key? key}) : super(key: key);
+
+  @override
+  _InfosScreenState createState() => _InfosScreenState();
+}
+
+class _InfosScreenState extends State<InfosScreen> {
+  final AirtableData4 airtableData = AirtableData4();
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[Text("I am a text")],
-      ),
-    );
+    return SizedBox(
+        height: 200,
+        child: FutureBuilder(
+          future: airtableData.getInfos(),
+          builder: (BuildContext context,
+              AsyncSnapshot<List<AirtableDataInfos>> snapshot) {
+            if (snapshot.hasData) {
+              List<AirtableDataInfos>? values = snapshot.data;
+              return ListView(
+                children: values!
+                    .map(
+                      (AirtableDataInfos value) => ListTile(
+                        title: Image.network(value.image),
+                        subtitle: Text(value.title),
+                        trailing: Text(value.texte),
+                      ),
+                    )
+                    .toList(),
+              );
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
+        ));
   }
 }
